b4e273a75a850b8defa5f63ee963684cebe8e091
joordy <jmfronik@gmail.com>
2021-01-28 01:01:46 +0100

    Adding tailwind-css to front-end section

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const express = require('express');
 const connectDB = require('./config/db');
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(express.json({ extended: false }));
-app.use('/', require('./routes/index'));
-app.use('/api/url', require('./routes/url'));
+app
+  .set('view engine', 'ejs')
+  // .set('views', 'view-ejs')
+  .use(express.static('static'))
+  .use(express.json({ extended: false }))
+  .use('/', require('./routes/index'))
+  .use('/api/url', require('./routes/url'));
+
+app.get('/', async (req, res) => {
+  res.render('index');
+});
 
 app.listen(port, () => console.log(`Server runs on ${port}`));
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  theme: {
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      borderColor: ['focus-visible'],
+      opacity: ['disabled'],
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
